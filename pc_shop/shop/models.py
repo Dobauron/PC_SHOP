deac3f1bbafa2ac84045b49e3c422f90f23e75c8
Dobauron <safetysafetystrazak@gmail.com>
2024-08-03 03:30:17 +0200

    migrations for model Product, Category

--- pc_shop/shop/models.py
+++ pc_shop/shop/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
