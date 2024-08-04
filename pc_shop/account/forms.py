e6cf48e5fc12dd994c8f98674456fdd378a93e21
Dobauron <safetysafetystrazak@gmail.com>
2024-08-04 04:02:08 +0200

    Authetication in progress

---
+++ pc_shop/account/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Account
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = Account
+        fields = ('email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(UserRegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.EmailField(label="Email")
--- pc_shop/account/views.py
+++ pc_shop/account/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import UserRegistrationForm, UserLoginForm
 
-# Create your views here.
+
+class UserRegisterView(FormView):
+    template_name = 'accounts/register.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('accounts:login')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    authentication_form = UserLoginForm
+
+
+class UserLogoutView(LogoutView):
+    template_name = 'accounts/logout.html'
