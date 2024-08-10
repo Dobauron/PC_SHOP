dcdf8caf8ac1887ba5d549d7055b55ddf7b214a7
Dobauron <safetysafetystrazak@gmail.com>
2024-08-10 23:44:16 +0200

    test for user_account models, views, forms work fine

--- pc_shop/user_account/forms.py
+++ pc_shop/user_account/forms.py
@@ -1,11 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Account
-from django.contrib.auth import get_user_model
 from allauth.account.forms import SignupForm
-from django.core.exceptions import ValidationError
-
-User = get_user_model()
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -19,10 +15,6 @@ class UserLoginForm(AuthenticationForm):
 
 
 class CustomSignupForm(SignupForm):
-    def save(self, commit=True):
-        user = super(CustomSignupForm, self).save(commit=False)
-        if commit:
-            user.save()
+    def save(self, request):
+        user = super().save(request)
         return user
-
-
