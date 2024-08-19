07ad7c929ecdad893bb9c7f02e819de053bab0ef
Dobauron <safetysafetystrazak@gmail.com>
2024-08-20 00:39:59 +0200

    remove context processor for cart, was unnecessary

--- cart/context_processors.py
+++
@@ -1,8 +0,0 @@
-from cart.models import Cart
-
-
-def cart(request):
-    if request.user.is_authenticated:
-        cart = Cart.objects.filter(user=request.user).first()
-        return {"cart": cart}
-    return {"cart": None}
